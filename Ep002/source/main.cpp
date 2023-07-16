716b29a286b609cd8bf777bff9ab18e89e9200f3
Mano Prakash P <manoprakash6@gmail.com>
2023-07-16 11:35:01 +0530

    Added Initial c++ code to try cmake

---
+++ Ep002/source/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <concepts>
+using namespace std;
+
+template <typename T>
+requires integral<T>
+T add( T a, T b){
+  return a + b;
+}
+
+int main()
+{
+  cout << "Hello from C++ on windows" << endl;
+  cout << "The sum is: " << add(7, 5) << endl;
+  return 0;
+}
